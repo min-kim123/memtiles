f7f2ce1b91796dbdec8396ccf8df43af6850cf39
Min Kim <8450minkim@gmail.com>
2023-07-06 11:02:56 -0700

    switch between dimensions finished

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-//5, 6, 7, 8, 9
-//after user gets all squares turn the grid entirelly green so that user can 
-//choose when to move on
 let round = 3;
-let across = 5;
 let count = 0;
 
 let highScore5 = 0;
@@ -15,13 +11,13 @@ function rand(max) {
     return Math.floor(Math.random() * max);
 }
 
-function buildTile(num) {
+function buildTile(num, across) {
     const element = document.createElement("div");
     element.classList.add("tile");
     element.setAttribute("number", num);
     element.setAttribute("bool", false);
     element.addEventListener("click", () => {
-      if (element.style.backgroundColor == "green") {//it is green and greenScreenClicks is 0, display blue tiles
+      if (element.style.backgroundColor == "green") {//it is green , display blue tiles
         //get rid of green
         let el = document.querySelectorAll(".tile");
         for (let i = 0; i < across*across; ++i) {
@@ -30,6 +26,7 @@ function buildTile(num) {
             el[i].style.setProperty('border', '1px solid #444444')
         }
         let a = document.querySelectorAll(`[bool="true"]`);
+        console.log(a)
         //localStorage.setItem('');
         for (let i = 0; i < round; ++i) {
             a[i].style.backgroundColor = "aqua"
@@ -41,21 +38,20 @@ function buildTile(num) {
           console.log(count);
           console.log("clicked");
           element.style.backgroundColor = "aqua";
+          element.setAttribute("bool", "false")
           if (count == round) {//all are clicked
             console.log("count equals round")
             round = round+1;
             console.log("round:" + round);
             document.getElementById("roundnum").innerHTML = round;
             count = 0;
-            //reset all true to false
-            let b = document.querySelectorAll(`[bool="true"]`);
-            console.log(b)
-            for (let i = 0; i < round-1; ++i) {
-              b[i].setAttribute("bool", "false");
-            }
-            setTiles();
-            greenScreen(round);
+
+            setTiles(across);
+            greenScreen(across);
           }
+      }
+      else if (element.getAttribute("bool") == "false" && element.style.backgroundColor != "aqua"){//not green, not right
+        element.style.backgroundColor = "red";
       }
       
     });
@@ -66,17 +62,18 @@ function call() {
     console.log("adfgh")
 }
 
-function greenScreen(round) {//display at start and between rounds
+function greenScreen(across) {//display at start and between rounds
   console.log("green screen")
   let el = document.querySelectorAll(".tile");
   for (let i = 0; i < across*across; ++i) {
       el[i].style.backgroundColor = "green"
-      el[i].style.setProperty('border', '1px')
+      el[i].style.setProperty('border', '1px solid #2d990c')
   }
 
 }
 
-function setTiles() {//set specified number of random tiles to true
+function setTiles(across) {//set specified number of random tiles to true
+  console.log('setTiles')
   let setarr = [];
   let arr = [];
   let x = 1;
@@ -101,48 +98,51 @@ function setTiles() {//set specified number of random tiles to true
   }
 }
 
-function build() {
-    
-    let greenScreenClicks = 0; 
-    
-    let tilesContainer = document.querySelector(".tiles");
-    tilesContainer.style.setProperty('grid-template-columns', 'repeat(' + across + ', 102px');
-
-    for (let i = 0; i <across*across; ++i) {
-        const tile = buildTile(i, round, greenScreenClicks);
-        tilesContainer.appendChild(tile);
-    } 
-    setTiles();
-    greenScreen(greenScreenClicks);
+function buildContainer(across) {
+  let tilesContainer = document.querySelector(".tiles");
+  while(tilesContainer.firstElementChild) {
+    tilesContainer.firstElementChild.remove();
+  }
+  console.log(across)
+  tilesContainer.style.setProperty('grid-template-columns', 'repeat(' + across + ', 102px');
+  for (let i = 0; i <across*across; ++i) {
+      const tile = buildTile(i, across);
+      tilesContainer.appendChild(tile);
+  } 
+  setTiles(across);
 }
 
-
-
+function build() {
+  let across = 5;
+  buildContainer(across);
+  document.querySelector("div.btn-dimension").addEventListener("click", function(evt){
+    if (evt.target.type === "radio") {
+      round = 3;
+      console.log(evt.target.value)
+      across = evt.target.value;
+      buildContainer(across);
+      greenScreen(across);
+    }
+  });
+  setTiles(across);
+  greenScreen(across);
+}
 document.querySelectorAll('.button')
   .forEach((button) => {
     //if ()
   });
 
 
-// let tilesContainer = document.querySelector(".tiles");
-// tilesContainer.style.setProperty('grid-template-columns', 'repeat(' + across + ', 102px')
-// let a = document.querySelectorAll(`[bool="true"]`);
+
 build();
 
 //localStorage.setItem('');
-/*
-for (let i = 0; i < round; ++i) {
-    a[i].style.backgroundColor = "aqua"
-}
-console.log(a)
 
-setTimeout(() => {
-    let a = document.querySelectorAll(`[bool="true"]`);
-    for (let i = 0; i < round; ++i) {
-    a[i].style.backgroundColor = "444444"
-}
+// setTimeout(() => {
+//     let a = document.querySelectorAll(`[bool="true"]`);
+//     for (let i = 0; i < round; ++i) {
+//     a[i].style.backgroundColor = "444444"
+// }
 
 
-}, 1000);
-
-//document.getElementById("roundnum").innerHTML = round;*/
+// }, 1000);
